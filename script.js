2cd43788cf5427bb1157e771d7e9e07bbc51bcaa
Oliver-asm <110045756+Oliver-asm@users.noreply.github.com>
2024-12-12 16:03:13 -0400

    Create the canvas of the program

---
+++ script.js
@@ -0,0 +1,18 @@
+const container = document.querySelector("#container");
+
+function createGrid(value=16){
+    for(let i= 1; i <= value; i++){
+        let div = document.createElement("div");
+        div.classList.add("rowDiv");
+        for(let i = 1; i <= value; i++){
+            let divElement = document.createElement("div");
+            divElement.classList.add("element");
+            
+            div.appendChild(divElement)
+        } 
+        container.appendChild(div);
+
+    }
+}
+
+createGrid();
