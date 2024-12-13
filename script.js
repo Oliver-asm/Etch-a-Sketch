91c2c5e6844cdfefacce9f574ced04ea9790fc95
Oliver-asm <110045756+Oliver-asm@users.noreply.github.com>
2024-12-13 15:50:18 -0400

    Complete project

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("button");
 
 function createGrid(value=16){
     for(let i= 1; i <= value; i++){
@@ -13,8 +14,22 @@ function createGrid(value=16){
         container.appendChild(div);
 
     }
-    
-    
 }
 
-createGrid(100);
+container.addEventListener("mouseover",(event)=> {
+    let element = event.target;
+    element.classList.add("backColor");
+});
+
+button.addEventListener("click",()=>{
+    let value = prompt("Insert the dimension of the canvas that you desise.");
+    if(value>0 && value<101){
+        container.textContent="";
+        createGrid(value);
+    } else{
+        alert("Values over 100 and below 1 are not allowed.")
+    }
+    
+})
+
+createGrid();
